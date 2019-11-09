c52f3ae36a089b900549a7f99142e0bbb5884225
AtsukiTak <takatomgoo@gmail.com>
2019-11-09 22:32:50 +0900

    builder: pass and play 01-parse test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,29 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(tokens: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(tokens as DeriveInput);
 
-    unimplemented!()
+    let data = match input.data {
+        Data::Struct(d) => d,
+        _ => panic!("Builder derive macro only supports struct."),
+    };
+
+    let fields = match data.fields {
+        Fields::Named(f) => f,
+        _ => panic!("Builder derive macro only supports named fields"),
+    };
+
+    let field_names = fields
+        .named
+        .iter()
+        .map(|f| f.clone().ident.unwrap().to_string())
+        .collect::<Vec<_>>();
+
+    eprintln!("fields: {:#?}", field_names);
+
+    TokenStream::new()
 }
